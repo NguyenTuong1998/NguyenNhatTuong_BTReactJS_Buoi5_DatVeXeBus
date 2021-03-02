93c432e47cdc830d6799611a5caf20ec12fae388
NguyenTuong1998 <tuongiuh122@gmail.com>
2021-03-02 23:37:04 +0700

    Tuong

---
+++ src/Components/gheItem.jsx
@@ -0,0 +1,40 @@
+import React, { Component } from "react";
+
+class GheItem extends Component {
+  state = {
+    isbooking: false,
+  };
+
+  addListOder = () => {
+      this.setState({
+          isbooking: !this.state.isbooking,
+      })
+    this.props.addToProductListOder(this.props.data);
+  };
+
+  render() {
+    return (
+      <div
+        onClick={this.addListOder}
+        className={` btn ${
+          this.state.isbooking
+            ? "btn btn-success mb-3"
+            : "btn btn-secondary mb-3"
+        } && ${
+          this.props.data.TrangThai
+            ? "btn btn-danger mb-3"
+            : "btn btn-secondary mb-3"
+        }`}
+        // className={` btn ${
+        //   this.props.data.TrangThai
+        //     ? "btn btn-danger mb-3"
+        //     : "btn btn-secondary mb-3"
+        // }`}
+      >
+        {this.props.data.SoGhe}
+      </div>
+    );
+  }
+}
+
+export default GheItem;
